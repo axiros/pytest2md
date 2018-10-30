6f7ba92a943be5b1187c3ac10fe96d584bd63322
Gunther Klessinger <gk@axiros.com>
2018-10-30 20:01:08 +0100

    readme

--- tests/test_tutorial.py
+++ tests/test_tutorial.py
@@ -33,6 +33,9 @@ class TestChapter1:
 
         <from-file: %s>
 
+        [Here](https://github.com/axiros/DevApps) is a bigger tutorial created
+        from `pytest_to_md` ([src](https://github.com/axiros/DevApps/tests/test_tutorial.py)).
+
         """
             % __file__
         )
