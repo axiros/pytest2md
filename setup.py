db700ba18cd3988f12af49da9a03274e1532f628
Gunther Klessinger <gk@axiros.com>
2019-04-17 22:11:58 +0200

    version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open(path.join(HERE, 'README.md')) as fd:
 
 setup(
     name='pytest_to_md',
-    version='20190421',
+    version='20190422',
     description='Create and run markdown Readmes from within pytest',
     long_description=md,
     long_description_content_type='text/markdown',
