2ac7edfb6987d8cd8326707f4099c121a2d9d6c1
gk <gk>
2019-04-17 22:10:55 +0200

    new_src_strcuture

---
+++ src/mdtool.py
@@ -0,0 +1,293 @@
+"""
+Postprocessing the Markdown for various Hosting Ways
+"""
+
+
+from functools import partial
+import fnmatch
+import sys, os
+
+# replacer for shortcut curls, which can be themselves replaced by the build
+# static is just a static webserver:
+known_src_links = {
+    'github': 'https://github.com/%(gh_repo_name)s/blob/%(git_rev)s/%(path)s%(line:#L%s)s',
+    'github_raw': 'https://raw.githubusercontent.com/%(gh_repo_name)s/%(git_rev)s/%(path)s%(line:#L%s)s',
+    'static': 'file://%(src_base_dir)s/%(path)s',
+}
+known_src_links['static_raw'] = known_src_links['static']
+
+
+def info(msg, **kw):
+    print(msg, kw)
+
+
+valid_file = lambda f: False if f.endswith('.pyc') else True
+valid_dir = lambda d: False if (d[0] in ('.', '_') or '.egg' in d) else True
+
+
+def find_file(pattern, path, match=fnmatch.fnmatch):
+    result = []
+    for root, dirs, files in os.walk(path, topdown=True):
+        dirs[:] = [d for d in dirs if valid_dir(d)]
+        for name in files:
+            if valid_file(name):
+                if match(name, pattern):
+                    result.append(os.path.join(root, name))
+    return result
+
+
+class InitData:
+    """
+    This is delivering values for keys in src_links which are not determinable
+    from the markdown itself but from e.g. looking into our git config.
+
+    Example: gh_repo_name is part of the links for github.
+
+    All keys used in the source link templates in 'known_src_links' must be
+    having a method here to find them, unless they can be delivered from the
+    markdown itself..
+
+    Todo: extend for bitbucket..., custom"""
+
+    @staticmethod
+    def src_base_dir(mdt):
+        return mdt.src_base_dir
+
+    @staticmethod
+    def gh_repo_name(mdt):
+        r = os.popen('cd "%s"; git remote -v' % mdt.src_base_dir).read()
+        r = [l for l in r.splitlines() if 'github.com' in l and 'push' in l]
+        if not r:
+            raise Exception('No github push remote found. remotes=' % str(r))
+        r = r[0].split('github.com:', 1)[1].rsplit('.git', 1)[-2]
+        return r
+
+    @staticmethod
+    def git_rev(mdt):
+        r = os.popen('cd "%s"; git rev-parse HEAD' % mdt.src_base_dir).read()
+        return r.strip()
+
+
+class ItemGetter:
+    def __init__(self, ctx):
+        self.ctx = ctx
+
+    def __getitem__(self, k, d=None):
+        c = self.ctx
+        v = c.pop(k, None)
+        if v:
+            return v
+        # %(line:#L%s)s
+        if ':' in k and '%s' in k:
+            k1, r = k.split(':', 1)
+            if k1 in c:
+                return r % c.pop(k1)
+        return ''
+
+
+class MDTool(object):
+    md = ''
+    links = {}
+    autogen_links_sep = '\n\n<!-- autogenlinks -->\n'
+
+    def __init__(self, md, src_dir, src_link_tmpl_name=None):
+        self._md_file = md
+
+        if not src_link_tmpl_name:
+            # try to find from within the md itself:
+            md = self._md_file.split('<!-- md_links_for:')
+            if len(md) > 1:
+                src_link_tmpl_name = md[1].split('--', 1)[0].strip()
+        src_link_tmpl_name = src_link_tmpl_name or 'static'
+        print('Rendering links for', src_link_tmpl_name)
+        self.src_base_dir = src_dir
+        self.src_link_tmpl_name = src_link_tmpl_name
+        if not os.environ.get('NOLINKREPL'):
+            self.src_link_repl_ctx = self.init_src_link_tmpl()
+
+    def init_src_link_tmpl(self):
+        """doing all we only have to do once"""
+
+        # mdt.src_link_tmpl = 'github'
+        # replace given by name with the lookup result from teh known..dict:
+        name = self.src_link_tmpl_name
+        self.src_link_tmpl = sl = known_src_links[name]
+        self.src_link_tmpl_raw = known_src_links[name + '_raw']
+
+        if not '/' in sl:
+            raise Exception('Not supported', self.src_link_tmpl)
+        ctx = self.src_url_ctx = {}
+        for k in [l for l in dir(InitData) if not l.startswith('_')]:
+            if k in sl:
+                # adding git revision to link rednering context
+                # k e.g. gh_repo_name
+                ctx[k] = getattr(InitData, k)(self)
+        return ctx
+
+    def do_set_links(mdt):
+        """
+        Link replacer.
+        Rewrites `[k1:v1,k2:v2,...]<SRC>` by replacing k1, k2,... in
+        `src_link_tmpl` keys with given values.
+
+        if a key is not in context the replacement is empty string.
+
+        Replacement can be 1 level nested: [line:#L%s] resolves e.g. to '#L42'
+        if line is present else to ''
+
+        Special keys:
+        - title: Will become the link text
+        - src_base_dir: From App, for pytest the folder of the pytested file.
+        - path: file path relative to src_base_dir
+        - fmatch: Startswith pattern of file name in dir. Must match uniquely.
+            also builds if not present:
+            - path
+            - title
+        - lmatch: Contains match within a file
+            also builds if not present:
+            - line
+        - gh_repo_name, git_rev: determined once at startup
+
+        Trivial format [foo]<SRC> is ident to [fmatch:foo]<SRC>
+
+        """
+        if os.environ.get('NOLINKREPL'):
+            info('Not replacing links', environ='NOLINKREPL')
+            return
+
+        md = mdorig = mdt._md_file.split(mdt.autogen_links_sep, 1)[0]
+        mdparts = md.split('\n```')
+        r = []
+        while mdparts:
+            md, fenced_code = mdparts.pop(0), None
+            if mdparts:
+                fenced_code = mdparts.pop(0)
+            parts = md.split(']<SRC>')
+            ri = []
+            while parts[:-1]:
+                part = parts.pop(0)
+                if not '[' in part:
+                    ri.append(part)
+                    continue
+                pre, lnk = part.rsplit('[', 1)
+
+                title, link = mdt.build_src_link(lnk)  # <--------------- !
+
+                if title != None:
+                    ri.append('%s[%s][%s]' % (pre, title, link))
+                else:
+                    ri.append(part)
+            assert len(parts) == 1
+            ri.append(parts[0])
+            r.append(''.join(ri))
+            if fenced_code:
+                r.append(fenced_code)
+        mdr = '\n```'.join(r)
+        links = mdt.links
+        if not links:
+            return mdorig
+
+        mdr += mdt.autogen_links_sep
+        for l in sorted(links.keys()):
+            mdr += '[%s]: %s\n' % (l, links[l])
+
+        return mdr, mdr != mdorig
+
+    def build_src_link(mdt, lnk):
+        """lnk the stuff in []"""
+        # build ld: link dict, {'title', 'file', evtl. lineof...}"""
+        if ':' in lnk or ',' in lnk:
+            try:
+                ld = to_dict(lnk)  # link dict
+            except:
+                return None, None
+        else:
+            ld = {'title': lnk, 'fmatch': lnk}
+
+        ld['title'] = ld.get('title', ld.get('fnmatch', lnk))
+
+        title = ld['title']
+        raw = ld.pop('show_raw', 0)
+        if raw:
+            tmpl = mdt.src_link_tmpl_raw
+        else:
+            tmpl = mdt.src_link_tmpl
+
+        if '%(path' in tmpl and not 'path' in ld:
+            path = find_path(ld, bd=mdt.src_base_dir)
+            if not path:
+                return None, None
+            ld['path'] = path
+
+        ld['file'] = ld.get('file', ld.get('path', '').rsplit('/', 1)[-1])
+        ld['fullpath'] = fn = os.path.join(mdt.src_base_dir, ld['path'])
+
+        if '%(line' in tmpl and not 'line' in ld:
+            if 'lmatch' in ld:
+                match = ld['lmatch']
+                with open(fn, 'r') as fd:
+                    for (i, line) in enumerate(fd):
+                        if match in line:
+                            ld['line'] = str(i)
+                            break
+
+        link = ld['file']
+        ctx = dict(mdt.src_link_repl_ctx)
+        ctx.update(ld)
+        mdt.links[link] = tmpl % ItemGetter(ctx)
+
+        return title, link
+
+    def make_toc(mdt, md):
+        """making a table of content, replacing "[TOC]" -else at the beginning"""
+        T = '\n[TOC]\n'
+        if not T in md:
+            md = T + md
+        pre, md = md.split(T)
+        from pytest_to_md import strutils
+
+        lines = md.splitlines()
+        toc = ['']
+        r = []
+        while lines:
+            line = lines.pop(0)
+            r.append(line)
+            for code in '```', '    ':
+                if line.startswith(code):
+                    while True:
+                        r.append(lines.pop(0))
+                        if r[-1].startswith(code):
+                            break
+            if line.startswith('#'):
+                lev, h = line.split(' ', 1)
+                toc.append(
+                    '    ' * (len(lev) - 1)
+                    + '- [%s](#%s)' % (h, strutils.slugify(h, delim='-'))
+                )
+        toc.append('')
+        toc.extend(r)
+        res = pre + '\n'.join(toc)
+        return res
+
+
+def find_path(ld, bd):
+    """link dict, base dir"""
+    file = ld.get('fmatch')
+    if not file:
+        return
+    found = find_file(file + '*', bd)
+    if len(found) == 0:
+        print('Not found', file, 'from', ld, 'builddir', bd)
+        return
+    elif len(found) > 1:
+        found = [f for f in found if f.rsplit('/', 1)[-1] == file]
+        if len(found) != 1:
+            return
+            # info('No unique source link found')
+    if len(found) == 1:
+        return found[0][len(bd) + 1 :]
+
+
+def to_dict(s):
+    l = [kv.strip().split(':', 1) for kv in s.split(',')]
+    return dict([(k.strip(), v.strip()) for k, v in l])
---
+++ src/pytest_to_md.py
@@ -0,0 +1,399 @@
+"""
+Creates markdown - while testing contained code snippets.
+
+"""
+import strutils
+import inspect
+import subprocess as sp
+import pytest
+import os
+import time
+import pdb
+from ast import literal_eval as ev
+
+import mdtool
+
+exists = os.path.exists
+abspath = os.path.abspath
+dirname = os.path.dirname
+
+if not hasattr(sp, 'getoutput'):
+    # adding the convenient PY3 getoutput to sp.
+    # hope correct (in plane, offline):
+    import subprocess as sp
+
+    def _(*a, **kw):
+        kw['stdout'] = sp.PIPE
+        kw['stderr'] = sp.PIPE
+        kw['shell'] = True
+        out, err = sp.Popen(*a, **kw).communicate()
+        if out.endswith('\n'):
+            out = out[:-1]
+        return out + err
+
+    sp.getoutput = _
+
+# also py2 compat:
+breakpoint = pdb.set_trace
+
+# contains the config, populated in setup:
+cfg = {}
+
+dflt_md_sep = '<!-- autogen tutorial -->'
+
+DIR = lambda d: abspath(dirname(d))
+
+
+def rpl(what, *with_):
+    for w in with_:
+        if not isinstance(w, (tuple, list)):
+            w = (w, '')
+        what = what.replace(w[0], w[1])
+    return what
+
+
+def setup(
+    fn_test_file, fn_target_md=None, md_sep=dflt_md_sep, fn_target_md_tmpl=None
+):
+    """
+    called at module level by pytest test module, which creates the md
+    e.g. fn_test_file=/Users/gk/GitHub/pytest_to_md/tests/test_tutorial.py
+
+    - fn_target_md: The final rendered result file. Default: HERE/README.md
+    - md_sep: A seperator where we fill in the mardkown from the pytest
+    - fn_target_md_tmpl: A template file, containing the static md around the seps.
+                    If not given we replace the content at every pytest run in
+                    fn_target_md itself.
+                    This is useful when you edit a lot in markdown and don't want
+                    clutter from the autogen result in that file.
+    Filenames relative to directory of fn_test_file or absolut.
+
+    In this setup function we generate e.g. this as config:
+    (Pdb) pp cfg (for a run of our own tutorial)
+    {'d_assets'         : '/data/root/pytest_to_md/tests/tutorial/',
+    'fn_md'             : '/data/root/pytest_to_md/tests/tutorial.md',
+    'fn_target_md'      : '/data/root/pytest_to_md/README.md.tmpl',
+    'here'              : '/data/root/pytest_to_md/tests',
+    'md_sep'            : '<!-- autogen tutorial -->',
+    'name'              : 'tutorial',
+    'fn_target_md_tmpl' : '/data/root/pytest_to_md/README.md.tmpl'}
+    """
+    cfg['md_sep'] = md_sep
+    cfg['here'] = here = DIR(fn_test_file)
+
+    # assuming tests in <base>/tests:
+    if not fn_target_md:
+        fn_target_md = here + '/../README.md'
+
+    if not fn_target_md.startswith('/'):
+        fn_target_md = abspath(cfg['here'] + '/' + fn_target_md)
+    fn = cfg['fn_target_md'] = fn_target_md
+
+    dflt_tmpl = fn.replace('.md', '.tmpl.md')
+    if os.path.exists(dflt_tmpl) and not fn_target_md_tmpl:
+        fn_target_md_tmpl = dflt_tmpl
+    if not fn_target_md_tmpl:
+        print(
+            'Replacing content in the final markdown, no template',
+            cfg['fn_target_md'],
+        )
+        fn_target_md_tmpl = cfg['fn_target_md']
+    cfg['fn_target_md_tmpl'] = fn_target_md_tmpl
+
+    fn = fn_target_md_tmpl
+    if not exists(fn):
+        print('Creating', fn)
+        with open(fn, 'w') as fd:
+            fd.write('\n'.join(('', md_sep, 'will be autogened.', md_sep, '')))
+
+    name = cfg['name'] = rpl(fn_test_file.rsplit('/', 1)[-1], 'test_', '.py')
+    cfg['d_assets'] = here + '/' + name + '/'
+
+    def create_empty_md_file(name):
+        here = cfg['here']
+        # create
+        fn_md = cfg['fn_md'] = here + '/' + name + '.md'
+        if exists(fn_md):
+            os.unlink(fn_md)
+        with open(fn_md, 'w') as fd:
+            fd.write('')
+        return fn_md
+
+    fn_md = create_empty_md_file(name)
+    # returns e.g.
+    # /data/root/pytest_to_md/tests /data/root/pytest_to_md/tests/tutorial.md
+    # pytest will create fn_md from scratch, which will be put at write_readme
+    # into the template, which will be after replacements go into md_target
+    return here, fn_md
+
+
+#
+#    def get_repo_base_url(dvcs):
+#        url = dvcs  # just for the error, once we have hg it'll be sane
+#        if dvcs == 'git':
+#            url = os.popen('git config --get remote.origin.url').read().strip()
+#            if 'github' in url:
+#                url, post = url.rsplit('.git', 1)
+#                assert not post
+#                pre, post = url.rsplit('/', 1)
+#                pre = pre.rsplit('/', 1)[-1].rsplit(':', 1)[-1]
+#                cfg['repo_path'] = pre + '/' + post
+#                return repo_urls['github']
+#        raise NotImplemented('Repo url for dvcs', dvcs, url)
+#
+#    def set_repo_base_url(rbu):
+#        dvcss = {'git': None, 'hg': None}
+#        if rbu == None:
+#            d = cfg['here']
+#            for dvcs in dvcss.keys():
+#                while not exists(d + '/.' + dvcs) and d != '/':
+#                    d = dirname(d)
+#                if d != '/':
+#                    dvcss[dvcs] = get_repo_base_url(dvcs)
+#                    break
+#
+#        breakpoint()
+#
+#    set_repo_base_url(repo_base_url)
+#
+# repo_urls = {
+#    'github': 'https://github.com/%(repo_path)s/blob/%(repo_rev)s/%(file_path)s#L%(file_line)s'
+# }
+#
+
+
+def write_readme(with_source_ref=False, make_toc=False):
+    """
+    addd the new version of the rendered tutorial into the main readme
+    """
+    # export DIR_SRC="https://github.com/axiros/DevApps/blob/`git rev-parse  HEAD`/"
+    # fn_md is created from the pytest:
+    src = sys._getframe().f_back.f_code
+    fn = cfg['fn_md']
+    with open(fn) as fd:
+        tut = fd.read()
+    if with_source_ref:
+        tut += dedent(
+            """
+
+        *Auto generated by [pytest_to_md](https://github.com/axiros/pytest_to_md), running [%s]<SRC>*
+        """
+            % src.co_filename.rsplit('/', 1)[-1]
+        )
+
+    fnr = cfg['fn_target_md_tmpl']
+    with open(fnr) as fd:
+        readm = fd.read()
+    # something like <! autoconf...:
+    m = cfg['md_sep']
+    pre, _, post = readm.split(m)
+    md = ''.join((pre, m, tut, '\n', m, post))
+    print('Now postprocessing', cfg['fn_target_md'])
+    d = dirname(cfg['fn_target_md'])
+    mdt = mdtool.MDTool(
+        md=md, src_dir=d, src_link_tmpl_name=os.environ.get('MD_LINKS_FOR')
+    )
+    md, changed = mdt.do_set_links()
+    if make_toc:
+        md = mdt.make_toc(md)
+    with open(cfg['fn_target_md'], 'w') as fd:
+        fd.write(md)
+
+
+code = """```code
+%s
+```"""
+# fmt: off
+nothing  = lambda s: s
+python   = lambda s: code.replace('code', 'python')   % s
+bash     = lambda s: code.replace('code', 'bash')     % s
+markdown = lambda s: code.replace('code', 'markdown') % (s.replace('```', "``"))
+as_lang  = lambda s, lang: code.replace('code', lang) % s
+# fmt: on
+
+import sys
+import inspect
+
+
+class rec_stdout:
+    h = []
+
+    def write(*a):
+        rec_stdout.h.extend(list(a))
+
+
+def run_func(funcstr, frame):
+    func = frame.f_locals[funcstr]
+    s = inspect.getsource(func).split(':', 1)[1]
+    s = deindent(s)
+    s = python(s)
+    try:
+        o = sys.stdout
+        sys.stdout = rec_stdout
+        rec_stdout.h.clear()
+        func()
+    finally:
+        sys.stdout = o
+    so = ''.join(rec_stdout.h)
+    if so:
+        if so.startswith('MARKDOWN:'):
+            so = so.replace('MARKDOWN:', '\n')
+        elif not so.lstrip().startswith('```'):
+            so = '\nOutput:\n```\n' + so + '\n```'
+        s += so
+    return s + '\n'
+
+
+def deindent(p):
+    pp = p.replace('\n', '')
+    ind = len(pp) - len(pp.lstrip())
+    if not ind:
+        return p
+    return '\n'.join(
+        [l[ind:] if not l[:ind].strip() else l for l in p.splitlines()]
+    )
+
+
+from textwrap import dedent
+
+
+def html_table(list, *headers, summary=None):
+    """ A tool which test pythons' may use to format their func results"""
+    p = '</td><td>'
+    row = lambda r: '<tr><td>' + r + '</td></tr>'
+    r = (row(p.join(headers)),)
+    for l in list:
+        r += (row(p.join(l)),)
+    r = '<table>\n' + '\n'.join(r) + '\n</table>'
+    if summary:
+        r = """<details>
+        <summary>%s</summary>
+        %s
+        </details>
+        """ % (
+            summary,
+            r,
+        )
+
+    return 'MARKDOWN:\n\n' + r
+
+
+def md_from_source_code():
+    src = sys._getframe().f_back
+    mdsrc = dedent(inspect.getsource(src.f_code).split(':', 1)[1])
+    apos = mdsrc.lstrip()[:3]  # ''' or """
+    parts = mdsrc.split('\n' + apos + '\n')
+    r = parts.pop(0)
+    while parts:
+        part = parts.pop(0)
+        if parts:
+            func = parts.pop(0)
+            try:
+                funcname = func.lstrip().split('def ', 1)[1].split('(', 1)[0]
+            except Exception as ex:
+                continue
+            part += '\npyrun: %s\n' % funcname
+        r += part
+    md(r, test_func=src)
+
+
+def md(paras, into=nothing, test_func=None):
+    """writes markdown"""
+    paras = [paras]
+    lctx = {}
+    lctx['in_code_block'] = False
+
+    paras = [deindent(p) for p in paras]
+    test_func = test_func or sys._getframe().f_back
+    parts = paras[0].split('pyrun: ')
+    after = parts.pop(0)
+    while parts:
+        part = parts.pop(0)
+        func, post = part.split('\n', 1)
+        after += run_func(func, test_func) + post
+
+    paras = [after]
+
+    def repl(l, lctx=lctx):
+        if '```' in l:
+            lctx['in_code_block'] = not lctx['in_code_block']
+
+        ff = '<from_file: '
+        if l.strip().startswith(ff):
+            pre, post = l.split(ff, 1)
+            fn, post = post.rsplit('>', 1)
+            if not fn.startswith('/'):
+                fn = cfg['d_assets'] + fn
+            if not exists(fn):
+                s = l
+            else:
+                with open(fn) as fd:
+                    s = fd.read().strip()
+                if fn.endswith('.py'):
+                    s = python(s)
+                else:
+                    s = code % s
+            l = pre + s + post
+        return l
+
+    r = '\n'.join([repl(l) for para in paras for l in para.splitlines()])
+    r = into(r)
+    fn = cfg['fn_md']
+    with open(fn, 'a') as fd:
+        fd.write('\n' + r)
+
+
+def bash_run(cmd, res_as=None, no_cmd_path=False, no_show_in_cmd=''):
+    """runs unix commands, then writes results into the markdown"""
+    if isinstance(cmd, str):
+        cmds = [{'cmd': cmd, 'res': ''}]
+    elif isinstance(cmd, list):
+        cmds = [{'cmd': c, 'res': ''} for c in cmd]
+    else:
+        cmds = cmd
+    orig_cmd = cmds[0]['cmd']
+    if not res_as and orig_cmd.startswith('python -c'):
+        res_as = python
+    D = cfg['d_assets']
+
+    for c in cmds:
+        cmd = c['cmd']
+        fncmd = cmd if no_cmd_path else (D + cmd)
+        # run it:
+        res = c['res'] = sp.getoutput(fncmd)
+        if no_show_in_cmd:
+            fncmd = fncmd.replace(no_show_in_cmd, '')
+        # .// -> when there is no_cmd_path we would get that, ugly:
+        # this is just for md output, not part of testing:
+        c['cmd'] = fncmd.replace(D, './').strip().replace('.//', './')
+
+    r = '\n\n'.join(['$ %(cmd)s\n%(res)s' % c for c in cmds])
+
+    md(r, into=res_as if res_as else bash)
+    return cmds
+
+
+def sh_file(fn, lang='python', content=None):
+    ex = exists(fn)
+    if not ex and not content:
+        raise Exception('not found', fn)
+    dn = dirname(fn)
+    if not exists(dn):
+        os.system('mkdir -p "%s"' % dn)
+    if content:
+        with open(fn, 'w') as fd:
+            fd.write(content)
+    else:
+        with open(fn) as fd:
+            content = fd.read()
+    FN = abspath(fn).rsplit('/', 1)[1]
+    content = ('$ cat "%s"' % FN) + '\n' + content
+    content = as_lang(content, lang)
+    md(content)
+
+
+def sh_code(func):
+    return md(python(inspect.getsource(func)))
+
+
+# .
